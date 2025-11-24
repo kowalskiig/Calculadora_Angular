aa8881336db08af18d2a7256997eba91a93b08fa
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-11-24 13:20:36 -0300

    adicioanr logica ao formulario

--- src/app/lista-compra/lista-compra.ts
+++ src/app/lista-compra/lista-compra.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-compra',
-  imports: [],
+  imports: [ FormsModule],
   templateUrl: './lista-compra.html',
   styleUrl: './lista-compra.css',
 })
 export class ListaCompra {
 
+  item: string = "";
+  
 }
